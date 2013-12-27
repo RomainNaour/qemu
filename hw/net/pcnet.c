b79a0f844212096e607654e06376c563baec8431
Romain Naour <romain.naour@openwide.fr>
2013-12-27 18:27:59 +0100

    pcnet: TXON is now forced clear when the command STOP is sent to the Lance processor.
    Signed-off-by: Romain Naour <romain.naour@openwide.fr>

--- hw/net/pcnet.c
+++ hw/net/pcnet.c
@@ -890,8 +890,9 @@ static void pcnet_stop(PCNetState *s)
 #ifdef PCNET_DEBUG
     printf("pcnet_stop\n");
 #endif
-    s->csr[0] &= ~0xffeb;
-    s->csr[0] |= 0x0014;
+    s->csr[0] &= ~0xfffb;
+    s->csr[0] |= 0x0004;
+    s->csr[3] = 0;
     s->csr[4] &= ~0x02c2;
     s->csr[5] &= ~0x0011;
     pcnet_poll_timer(s);
